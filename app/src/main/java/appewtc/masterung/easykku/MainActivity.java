8f02f32aadd6a65a736125cf3ce3c706cdc57ecc
piyawat <salapao45@gmail.com>
2016-11-13 09:01:33 +0700

    Initial commit

---
+++ app/src/main/java/appewtc/masterung/easykku/MainActivity.java
@@ -0,0 +1,35 @@
+package appewtc.masterung.easykku;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private Button signInButton, signUpButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+
+        //Sign Up Controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+
+    }    // Main Method
+
+}   // Main Class นี่คือ คลาสหลัก
---
+++ app/src/main/java/appewtc/masterung/easykku/MyAlert.java
@@ -0,0 +1,42 @@
+package appewtc.masterung.easykku;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by masterUNG on 11/12/2016 AD.
+ */
+
+public class MyAlert {
+
+    //Explicit
+    private Context context;
+    private int anInt;
+    private String titleString, messageString;
+
+    public MyAlert(Context context, int anInt, String titleString, String messageString) {
+        this.context = context;
+        this.anInt = anInt;
+        this.titleString = titleString;
+        this.messageString = messageString;
+    }
+
+    public void myDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(anInt);
+        builder.setTitle(titleString);
+        builder.setMessage(messageString);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }   // myDialog
+
+}   // Main Class
